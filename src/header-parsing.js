14034e4a83e708456d1d7e7e155372771f296833
TrueJu <julien33widmer@gmail.com>
2022-12-29 20:20:54 +0100

    Added regex for counting individuals inside file

--- src/header-parsing.js
+++ src/header-parsing.js
@@ -5,6 +5,7 @@ var parsing_utils_1 = require("./parsing-utils");
 function parseHeader(rawGedcom) {
     var extractedHeaderInformation = (0, parsing_utils_1.getEntryByTopLevelName)(rawGedcom, 'head');
     var parsedGedcomHeader = (0, parsing_utils_1.gedcomToObject)(extractedHeaderInformation);
-    console.log(parsedGedcomHeader);
+    var numOfIndividuals = (rawGedcom.match(/0 @I[\d*|.*]@ INDI/g) || []).length;
+    console.log(numOfIndividuals);
 }
 exports.parseHeader = parseHeader;
--- src/header-parsing.ts
+++ src/header-parsing.ts
@@ -3,6 +3,5 @@ import {gedcomToObject, getEntryByTopLevelName} from './parsing-utils';
 export function parseHeader(rawGedcom:string) {
     const extractedHeaderInformation = getEntryByTopLevelName(rawGedcom, 'head');
     const parsedGedcomHeader = gedcomToObject(extractedHeaderInformation);
-
-    console.log(parsedGedcomHeader);
+    const numOfIndividuals = (rawGedcom.match(/0 @I[\d*|.*]@ INDI/g) || []).length;
 }
